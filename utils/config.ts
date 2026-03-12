1488f088760f314fea67193ee5b3f71ca01088de
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2026-03-11 23:03:48 -0300

    Corrigindo

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080' 
-//'https://api.csb.app.br'
+export const API_BASE_URL = 'https://api.csb.app.br'
 export const BUCKET_BASE_URL = 'https://csb.app.br/'
