e78162e71016d82253ce3e7c6cc4d399aa1c5d7c
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2026-04-15 20:55:58 -0300

    :/

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://152.67.62.244:80'
+export const API_BASE_URL = //'http://152.67.62.244:80'
 //'http://localhost:8080' 
-//'https://api.csb.app.br'
+'https://api.csb.app.br'
 export const BUCKET_BASE_URL = 'https://csb.app.br/'
