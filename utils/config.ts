6dd3c29e10ef0316aa9b608dc728f53b8a90198b
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2026-04-15 19:54:25 -0300

    alterando endpoint

--- utils/config.ts
+++ utils/config.ts
@@ -1,2 +1,4 @@
-export const API_BASE_URL = 'https://api.csb.app.br'
+export const API_BASE_URL = 'http://152.67.62.244:80'
+//'http://localhost:8080' 
+//'https://api.csb.app.br'
 export const BUCKET_BASE_URL = 'https://csb.app.br/'
